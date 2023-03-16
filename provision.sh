782aeb1754dbba0ff5836ec9bf0dd365b6b5821e
Yago Alexandre <goltarayago@gmail.com>
2023-03-16 12:19:47 -0300

    Creating VM and provisioning it

--- provision.sh
+++ provision.sh
@@ -4,7 +4,7 @@ sudo yum install -y yum-utils
 echo "Downloading Docker Engine"
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 echo "Installing Docker CE"
-sudo yum -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-echo "Starting Docker Service"
+sudo yum -y install docker-ce docker-ce-cli containerd.io
+echo "Enabling and dtarting Docker Service"
 sudo systemctl start docker
 sudo systemctl enable docker
