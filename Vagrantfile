fcc00718badd18bed756309cca69b16c1a1d5c22
Yago Alexandre <goltarayago@gmail.com>
2023-03-20 09:25:09 -0300

    Changing VM image from centos7 to almalinux8

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,5 @@
 Vagrant.configure("2") do |config|
-  config.vm.box = "centos/7"
-  config.vm.hostname = "docker"
+  config.vm.box = "almalinux/8"
+  config.vm.hostname = "docker-lab"
   config.vm.provision "shell", path: "provision.sh"
 end
--- provision.sh
+++ provision.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 echo "Installing yum-utils"
-sudo yum install -y yum-utils
+sudo dnf install -y yum-utils
 echo "Downloading Docker Engine"
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 echo "Installing Docker CE"
-sudo yum -y install docker-ce docker-ce-cli containerd.io
+sudo dnf -y install docker-ce docker-ce-cli containerd.io
 echo "Enabling and dtarting Docker Service"
 sudo systemctl start docker
 sudo systemctl enable docker
+
+echo "Adding common user to docker group"
+sudo usermod -aG docker $USER
+sudo systemctl restart docker
